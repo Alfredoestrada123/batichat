59d413b9421de4f4f7a2af3700fe477d128bf2ff
Alfredoestrada123 <75652521+Alfredoestrada123@users.noreply.github.com>
2021-11-08 20:44:53 -0600

    Update sw-aux.js

--- js/sw-aux.js
+++ js/sw-aux.js
@@ -1,8 +1,8 @@
 //amacenar en cache dinamico
-function actualizarCacheDinamico(DYNAMIC_CACHE, reqrequest, res) {
+function actualizarCacheDinamico(DYNAMIC_CACHE, request, res) {
   if (res.ok) {
     return caches.open(DYNAMIC_CACHE).then((cache) => {
-      cache.put(reqrequest, res.clone());
+      cache.put(request, res.clone());
       return res.clone();
     });
   } else {
